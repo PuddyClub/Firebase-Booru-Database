100c8fc8bd596969e9f1e05a83eeaec14052b4b6
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-13 12:31:45 -0300

    add Tag Function

--- index.js
+++ index.js
@@ -340,8 +340,12 @@ class booru_manager {
     // Add Items
 
     // Tags
-    addTagItem(tagName, itemID, itemData) {
+    addTagItem(tagName, itemID, itemData, allowPath = false) {
         return new Promise(function (resolve, reject) {
+
+            // Get Allow Path
+            let allowDatabasePath = false;
+            if (typeof allowPath === "boolean") { allowDatabasePath = allowPath; }
 
             // Obj Type
             const objType = require('@tinypudding/puddy-lib/get/objType');
@@ -358,7 +362,46 @@ class booru_manager {
                         // Validate Size
                         if (require('json-sizeof')(itemData) <= this.byteLimit.json.tag) {
 
+                            // Firebase Escape
+                            const databaseEscape = require('@tinypudding/puddy-lib/firebase/databaseEscape');
+
+                            const escapedTagName = databaseEscape(tagName, allowDatabasePath);
+                            const escapedItemID = databaseEscape(itemID, allowDatabasePath);
+
+                            // Get Tag
+                            const tagItem = this.dbItems.tag.child(escapedTagName).child(escapedItemID);
+
+                            // Set Data
+                            tagItem.set(itemData)
                             
+                            // Success
+                            .then(() => {
+
+                                // Send Result
+                                resolve({
+                                    db: tagItem,
+                                    values: {
+                                        normal: {
+                                            tag: tagName,
+                                            itemID: itemID
+                                        },
+                                        escape: {
+                                            tag: escapedTagName,
+                                            itemID: escapedItemID
+                                        }
+                                    }
+                                });
+
+                                // Complete
+                                return;
+
+                            })
+                            
+                            // Error
+                            .catch(err => {
+                                reject(err);
+                                return;
+                            });
 
                         }
 
